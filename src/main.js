180bc24f91008d8b0d8fb6a9eff547f89318004e
ZQakane <zhenq021@gmail.com>
2020-03-27 14:34:54 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,83 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "W", logoType: "text", url: "https://www.w3.org" },
+  { logo: "G", logoType: "text", url: "https://github.com" },
+  {
+    logo: "P",
+    logoType: "text",
+    url: "https://www.pinterest.com"
+  },
+  {
+    logo: "I",
+    logoType: "text",
+    url: "https://www.iconfont.cn"
+  },
+  { logo: "F", logoType: "text", url: "https://www.figma.com" }
+];
+
+const simplifyUrl = url => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, "");
+};
+
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+            <div class='site'>
+                <div class='logo'>${simplifyUrl(node.url)[0]}</div>
+                <div class='link'>${simplifyUrl(node.url)}</div>
+                <div class='close'>
+                <svg class="icon">
+                <use xlink:href="#icon-login"></use>
+            </svg>
+                </div>
+            </div>
+        </a>
+    </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", e => {
+      e.stopPropagation();
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+$(".addButton").on("click", () => {
+  let url = window.prompt("请输入需要添加的网址。");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  hashMap.push({
+    logo: simplifyUrl(url)[0],
+    logoType: "text",
+    url: url
+  });
+
+  render();
+});
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+$(document).on('keypress', (e) => {
+  const { key } = (e)
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase === key) {
+      window.open(hashMap[i].url);
+    }
+  }
+});
